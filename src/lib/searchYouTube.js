c72424a5b248401c808d976790f679a96e36cde1
Tim Anderson <tdanderson805@gmail.com>
2020-12-04 19:58:36 -0800

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
+
 var searchYouTube = (options, callback) => {
   // TODO
+  // todo: save a message to the server
+    $.ajax({
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      type: 'GET',
+      data: { 
+        key: options.key,
+        q: options.query,
+        part: 'snippet',
+        maxResults: options.max,
+        type: 'video',
+        videoEmbeddable: true
+      },
+      success: (data)=>{callback(data.items);},
+      error: function(error) {
+        console.error('YouTube Failed to fetch messages', error);
+      }
+    });
 };
 
 export default searchYouTube;
